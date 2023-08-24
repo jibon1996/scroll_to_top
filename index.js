8ad2684923ee4642c1c3ebb525202fcaa9441793
jibon <jibonkhan53296@gmail.com>
2023-08-24 09:01:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// back to top scroll js 
+let toTop = document.querySelector("#back-to-top");
+
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add("active")
+    }else{
+        toTop.classList.remove("active")
+    }
+})
